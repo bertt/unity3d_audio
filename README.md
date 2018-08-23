3a4e24dae07b8595edc51ae32a6a1defefd13c53
bert <btemme@hotmail.com>
2018-08-23 09:53:42 +0200

    initial

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # unity3d_audio
+
+Sample of using audio (recording, playing wav files) in Unity3D.
+
+Used objects:  WWW, AudioCLip en AudioSource
---
+++ src/Assets/AudioRecord.cs
@@ -0,0 +1,48 @@
+﻿using JetBrains.Annotations;
+using System.IO;
+using UnityEngine;
+
+public class AudioRecord : MonoBehaviour {
+    AudioClip myAudioClip;
+
+    void Start() { }
+    void Update() { }
+
+    [UsedImplicitly]
+    void OnGUI()
+    {
+        if (GUI.Button(new Rect(50, 10, 200, 100), "Record"))
+        {
+            Debug.Log("utrecht recording!");
+
+            var devices =Microphone.devices;
+            myAudioClip = Microphone.Start(null, false, 10, 44100);
+
+        }
+        if (GUI.Button(new Rect(50, 150, 200, 100), "Save"))
+        {
+            Debug.Log("utrecht saving!");
+            // SavWav.TrimSilence(myAudioClip,2);
+            SavWav.Save("myfile", myAudioClip);
+        }
+        if (GUI.Button(new Rect(50, 260, 200, 100), "Play"))
+        {
+            Debug.Log("utrecht playing!");
+            // SavWav.TrimSilence(myAudioClip,2);
+            var filename = "myfile.wav";
+            var filepath = Path.Combine(Application.persistentDataPath, filename);
+            filepath = @"C:\Users\bertt\myfile.wav";
+            WWW audioLoader = new WWW(filepath);
+            while (!audioLoader.isDone)
+            {
+                Debug.Log("uploading");
+            }
+            var audioclip = audioLoader.GetAudioClip();
+            var audioSource = GameObject.Find("Audio").GetComponentInChildren<AudioSource>();
+            audioSource.clip = audioclip;
+            audioSource.Play();
+        }
+
+
+    }
+}
