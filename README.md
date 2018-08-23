49b3a4a1ac7cd22048df43203512096af8d7b33c
bert <btemme@hotmail.com>
2018-08-23 13:12:09 +0200

    tested on android

--- README.md
+++ README.md
@@ -2,4 +2,14 @@
 
 Sample of using audio (recording, playing wav files) in Unity3D.
 
-Used objects:  WWW, AudioCLip en AudioSource
+Used objects:  WWW, AudioClip en AudioSource
+
+Features:
+
+- 2 buttons, for recording and playing audio
+
+- Record audio using events OnPointerClick, stop recording at OnPointerUp 
+
+- Record audio in wav format, store wav file on local file storage (Application.persistentDataPath)
+
+- Android support tested
--- src/Assets/AudioRecord.cs
+++ src/Assets/AudioRecord.cs
@@ -1,48 +1,25 @@
-﻿using JetBrains.Annotations;
-using System.IO;
+﻿using Audio;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
-public class AudioRecord : MonoBehaviour {
-    AudioClip myAudioClip;
-
-    void Start() { }
-    void Update() { }
-
-    [UsedImplicitly]
-    void OnGUI()
+namespace Assets
+{
+    public class AudioRecord : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
     {
-        if (GUI.Button(new Rect(50, 10, 200, 100), "Record"))
-        {
-            Debug.Log("utrecht recording!");
+        AudioClip audioClip;
 
-            var devices =Microphone.devices;
-            myAudioClip = Microphone.Start(null, false, 10, 44100);
+        public void OnPointerDown(PointerEventData eventData)
+        {
+            audioClip = Microphone.Start(null, false, 10, 44100);
 
-        }
-        if (GUI.Button(new Rect(50, 150, 200, 100), "Save"))
-        {
-            Debug.Log("utrecht saving!");
-            // SavWav.TrimSilence(myAudioClip,2);
-            SavWav.Save("myfile", myAudioClip);
-        }
-        if (GUI.Button(new Rect(50, 260, 200, 100), "Play"))
-        {
-            Debug.Log("utrecht playing!");
-            // SavWav.TrimSilence(myAudioClip,2);
-            var filename = "myfile.wav";
-            var filepath = Path.Combine(Application.persistentDataPath, filename);
-            filepath = @"C:\Users\bertt\myfile.wav";
-            WWW audioLoader = new WWW(filepath);
-            while (!audioLoader.isDone)
-            {
-                Debug.Log("uploading");
-            }
-            var audioclip = audioLoader.GetAudioClip();
-            var audioSource = GameObject.Find("Audio").GetComponentInChildren<AudioSource>();
-            audioSource.clip = audioclip;
-            audioSource.Play();
+            GetComponentInChildren<Text>().text = "Is recording...";
         }
 
-
+        public void OnPointerUp(PointerEventData eventData)
+        {
+            AudioLib.Save("test.wav", audioClip);
+            GetComponentInChildren<Text>().text = "Start recording";
+        }
     }
 }
